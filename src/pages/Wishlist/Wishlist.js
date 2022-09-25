ca3dbce44308405b00a0e22a20229889f263d8be
Milad Sadeghi <miladsadeghi2323@gmail.com>
2022-09-25 22:38:18 +0330

    message for empty wishlist

--- src/pages/Wishlist/Wishlist.js
+++ src/pages/Wishlist/Wishlist.js
@@ -5,7 +5,7 @@ import styles from "./Styles.module.scss";
 import { useNavigate } from 'react-router-dom';
 import { NavigateNext, AddShoppingCart, Close } from '@mui/icons-material';
 import { FaFacebookF, FaTwitter, FaInstagram, FaYoutube, FaPinterest } from "react-icons/fa";
-
+import emptyCart from "images/shopping-bag.png"
 const Wishlist = () => {
   const data = useContext(DataContext);
   const navigate = useNavigate();
@@ -25,86 +25,96 @@ const Wishlist = () => {
 
   return (
     <>
-      <div className={styles.bg}>
-        <h5>Wishlist</h5>
-        <h6>Shop</h6>
-      </div>
-      <Container>
-        <div className={styles.breadcrumb}>
-          <div role="presentation" onClick={handleClick}>
-            <Breadcrumbs aria-label="breadcrumb" separator={<NavigateNext fontSize="small" />} sx={{p: "14px 10px", mb: "40px"}}>
-              <Link 
-                className={styles.brdHov}
-                underline="none" 
-                color="inherit" 
-                href="/"
-                fontSize={"14px"} >
-                Home
-              </Link>
-              <Link
-                underline="none"
-                color="text.primary"
-                href="/wishlist"
-                aria-current="page"
-                fontSize={"14px"} >
-                Wishlist
-              </Link>
-            </Breadcrumbs>
-          </div>
+    {
+      data[4].length === 0 ?
+        <div className={styles.emptyCart}>
+          <img src={emptyCart} alt="empty_cart" />
+          <p>You must have a wishlist! do not have one?</p>
         </div>
-        <div>
-          <table className={styles.table}>
-            <thead>
-              <tr>
-                <th>Product</th>
-                <th>Price</th>
-                <th></th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                data[4].map(item => 
-                  <tr key={item}>
-                    <td>
-                      <div className={styles.product}>
-                        <div className={styles.imgDiv}>
-                          <img src={data[0][item].urls[0]} alt="product_photo" className={styles.imgCard} />
-                        </div>
-                        <h3>{data[0][item].title}</h3>
-                      </div>
-                    </td>
-                    <td>
-                      <h4 className={styles.price}>${data[0][item].price}</h4>
-                    </td>
-                    <td className={styles.addToCardBtn}>
-                      <button>
-                        <AddShoppingCart />
-                        ADD TO CART
-                      </button>
-                    </td>
-                    <td>
-                      <div className={styles.deleteBtn} onClick={deleteFromWishlist} id={data[0][item].id}>
-                        <Close  />
-                      </div>
-                    </td>
-                  </tr>
-                )
-              }
-            </tbody>
-          </table>
-          <div className={styles.socials}>
-            <p>Share on:</p>
-            <div className={styles.socialsIcon}>
-              <div><FaFacebookF /></div>
-              <div><FaTwitter /></div>
-              <div><FaInstagram /></div>
-              <div><FaYoutube /></div>
-              <div><FaPinterest /></div>
+        :
+      <>
+        <div className={styles.bg}>
+          <h5>Wishlist</h5>
+          <h6>Shop</h6>
+        </div>
+        <Container>
+          <div className={styles.breadcrumb}>
+            <div role="presentation" onClick={handleClick}>
+              <Breadcrumbs aria-label="breadcrumb" separator={<NavigateNext fontSize="small" />} sx={{p: "14px 10px", mb: "40px"}}>
+                <Link 
+                  className={styles.brdHov}
+                  underline="none" 
+                  color="inherit" 
+                  href="/"
+                  fontSize={"14px"} >
+                  Home
+                </Link>
+                <Link
+                  underline="none"
+                  color="text.primary"
+                  href="/wishlist"
+                  aria-current="page"
+                  fontSize={"14px"} >
+                  Wishlist
+                </Link>
+              </Breadcrumbs>
             </div>
           </div>
-        </div>
-      </Container>
+          <div>
+            <table className={styles.table}>
+              <thead>
+                <tr>
+                  <th>Product</th>
+                  <th>Price</th>
+                  <th></th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  data[4].map(item => 
+                    <tr key={item}>
+                      <td>
+                        <div className={styles.product}>
+                          <div className={styles.imgDiv}>
+                            <img src={data[0][item].urls[0]} alt="product_photo" className={styles.imgCard} />
+                          </div>
+                          <h3>{data[0][item].title}</h3>
+                        </div>
+                      </td>
+                      <td>
+                        <h4 className={styles.price}>${data[0][item].price}</h4>
+                      </td>
+                      <td className={styles.addToCardBtn}>
+                        <button>
+                          <AddShoppingCart />
+                          ADD TO CART
+                        </button>
+                      </td>
+                      <td>
+                        <div className={styles.deleteBtn} onClick={deleteFromWishlist} id={data[0][item].id}>
+                          <Close  />
+                        </div>
+                      </td>
+                    </tr>
+                  )
+                }
+              </tbody>
+            </table>
+            <div className={styles.socials}>
+              <p>Share on:</p>
+              <div className={styles.socialsIcon}>
+                <div><FaFacebookF /></div>
+                <div><FaTwitter /></div>
+                <div><FaInstagram /></div>
+                <div><FaYoutube /></div>
+                <div><FaPinterest /></div>
+              </div>
+            </div>
+          </div>
+        </Container>
+      </>
+    }
     </>
   );
 }
